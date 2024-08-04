779fed8395d1b4f3ade3b91313429ec8f4adb6dc
Daksh Patel <dakshpdpu@gmail.com>
2024-08-04 11:59:32 +0530

    updated the pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'react-app',
-        script: 'npm ',
-        args: 'run serve',
+        script: 'npm run serve',
         env: {
           PM2_SERVE_PORT: 4173,
           PM2_SERVE_PATH: './dist',
